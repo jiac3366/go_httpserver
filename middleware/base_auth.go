0095318c875c8e08950ae2422976837b6bb9a9f8
jiac3366 <463045792@qq.com>
2021-11-28 17:01:33 +0800

    add k8s file

--- middleware/base_auth.go
+++ middleware/base_auth.go
@@ -8,8 +8,8 @@ import (
 func BasicAuth() gin.HandlerFunc {
 
 	// 从Secret中获取
-	name := os.Getenv("name")
-	pwd := os.Getenv("pwd")
+	name := os.Getenv("name")  //jiaccc
+	pwd := os.Getenv("pwd")   // 3366
 	return gin.BasicAuth(gin.Accounts{
 		name: pwd,
 	})
