e5b97e61ab34352354a060a074c5343641af4924
jiac3366 <463045792@qq.com>
2021-12-28 13:34:31 +0800

    service3

--- handlers/tracing_handler.go
+++ handlers/tracing_handler.go
@@ -6,8 +6,6 @@ import (
 	"github.com/golang/glog"
 	"io"
 	"math/rand"
-	"net/http"
-	"strings"
 	"time"
 )
 
@@ -35,28 +33,9 @@ func TracingHandler(ctx *gin.Context) {
 	w := ctx.Writer //!!
 	io.WriteString(w, "===================Details of the http request header:============\n")
 	fmt.Printf("===================Details of the http request header:============\n")
-	req, err := http.NewRequest("GET", "http://service2", nil)
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-	lowerCaseHeader := make(http.Header)
-	r := ctx.Request //!!
-	for key, value := range r.Header {
-		lowerCaseHeader[strings.ToLower(key)] = value
-	}
-	glog.Info("headers:", lowerCaseHeader)
-	req.Header = lowerCaseHeader
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		glog.Info("HTTP get failed with error: ", "error", err)
-		fmt.Printf("HTTP get failed with error: \n")
-	} else {
-		glog.Info("HTTP get succeeded")
-		fmt.Printf("HTTP get succeeded \n")
-	}
-	if resp != nil {
-		resp.Write(w) // ??往回发？
+	r := ctx.Request
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
 	}
 	glog.V(4).Infof("Respond in %d ms", delay)
 	fmt.Printf("Respond in %d ms\n", delay)
